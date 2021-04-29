23452b8d340f03dff02f5f11b328dd09cbd09378
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-04-29 16:40:13 +0500

    add: Nx Workspace

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'micro-frontend-module-federation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shell';
+}
